2cf073bd709c41adb6512bf25d69a6cf3592286f
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-11-08 01:51:26 -0300

    modulo 01
    sub modulo react + webpack

--- anotacoes.js
+++ anotacoes.js
@@ -6,6 +6,19 @@
 // github issues : dúvida -> escreve a dúvida e marca com @fdaciuk
 
 ---------------------------------------------------------------------- MÓDULO 01 -----------------------------------------------------------------------
-
 $ npm i http-server // ou yarn add
 $ http-server // iniciar o http-server
+
+
+---------- SUB MÓDULO WEBPACK & REACT -----------
+webpack.github.io
+
+// sempre que for instalar as dependências, ver no repositório do curso quais são as versões que foram instaladas nas gravações dos vídeos !
+"https://github.com/da2k/curso-reactjs-ninja/blob/2209a8e5c54436d124fbcb87c05ae88ef2ba2dcb/versions-of-dependencies.md"
+
+// após as configurações inicias, é necessário criar o arquivo bundle.js
+// após instalar de forma global o webpack: 
+$ webpack // este comando vai gerar o arquivo bundle
+
+// após instalar webpack-dev-server de forma global
+$ webpack-dev-server // vai executar o servidor de desenvolvimento do webpack
