3532c36afee8f28ec46ee7a558d0624ed656d5e4
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-11-08 15:09:03 -0300

    modulo 01
    stateful vs stateless

--- m01/learning-state/src/app.js
+++ m01/learning-state/src/app.js
@@ -1,22 +1,31 @@
 'use strict'
 import React, { Component } from 'react'
 
+import Button from './button'
+import Square from './square'
+
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      text: 'Murilo'
+      color: 'gray'
     }
   }
 
   render() {
+    const colors = ['red', 'green', 'blue', 'black', 'gold']
+
     return (
-      <div className="container" onClick={
-        () => this.setState({
-          text: 'Outro texto'
-        })
-      }>
-        {this.state.text}
+      <div>
+        <Square color={this.state.color} />
+        {colors.map(color => (
+          <Button
+            key={color}
+            handleClick={() => this.setState({ color })}
+          >
+            {color}
+          </Button>
+        ))}
       </div>
     )
   }
