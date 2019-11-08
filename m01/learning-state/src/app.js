4eb1f0c92c74dfa353a620c24b36e27209f6e271
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-11-08 14:30:25 -0300

    modulo 01
    learning state

---
+++ m01/learning-state/src/app.js
@@ -0,0 +1,25 @@
+'use strict'
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      text: 'Murilo'
+    }
+  }
+
+  render() {
+    return (
+      <div className="container" onClick={
+        () => this.setState({
+          text: 'Outro texto'
+        })
+      }>
+        {this.state.text}
+      </div>
+    )
+  }
+}
+
+export default App
