9f212ada53a48af1da5b54e6a1f1bd4865616e27
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-11-08 13:32:39 -0300

    modulo 01
    props children

--- m01/learning-events/src/app.js
+++ m01/learning-events/src/app.js
@@ -1,11 +1,15 @@
 'use strict'
 import React, { Component } from 'react'
+import Button from './button'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        App
+        <Button>
+          <span>Texto</span><br />
+          outro texto
+        </Button>
       </div>
     )
   }
