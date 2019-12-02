5064d0a8525bca0e16242e04d33509e2d6db9e3f
cunhamuril <murilo.sant@hotmail.com>
2019-12-02 15:36:58 -0300

    modulo 01
    trabalhando com libs de terceiros

---
+++ m01/3rd-party-with-react/src/app.js
@@ -0,0 +1,13 @@
+'use strict'
+import React, { Component } from 'react'
+import Plugin from './plugin'
+
+class App extends Component {
+  render() {
+    return (
+      <Plugin />
+    )
+  }
+}
+
+export default App
