4fb3d3385bff41befbfcabf794fca308e90c8700
cunhamuril <murilo.sant@hotmail.com>
2020-01-29 16:58:39 -0300

    modulo 02 - TDD
    Every

---
+++ m02/jest-in-practice/src/every/every.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import { expect } from "chai";
+import every from './every'
+
+it('every should be a function', () => {
+  expect(every).to.be.a('function')
+});
+
+/**
+ * sempre que for testar valores booleanos, 
+ * utiliza to.be.ok para true ou to.not.be.ok para false
+ */
+
+it('every([], item => item) should return true', () => {
+  expect(every([], item => item)).to.be.ok
+})
+
+it('every([0, 2, 3], item => item) should return false', () => {
+  expect(every([0, 2, 3], item => item)).to.not.be.ok
+})
+
+it('every([1, 2, 3], item => item) should return true', () => {
+  expect(every([1, 2, 3], item => item)).to.be.ok
+})
+
+it('every([1, 2, 3], (item, index) => index === item - 1) should return true', () => {
+  expect(every([1, 2, 3], (item, index) => index === item - 1)).to.be.ok
+})
+
+it('every([1, 2, 3], (item, index, array) => array.length === 3) should return true', () => {
+  expect(every([1, 2, 3], (item, index, array) => array.length === 3)).to.be.ok
+})
