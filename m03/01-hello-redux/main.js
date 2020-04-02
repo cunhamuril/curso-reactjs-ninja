82cb93c7475b23fbf170809f9c9d41ad510c75d9
cunhamuril <murilo.sant@hotmail.com>
2020-04-02 13:34:58 -0300

    feat: 🎸 primeiro contato com redux
    estrutura básica

---
+++ m03/01-hello-redux/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// console.asserte: Função do JS para testes.
+// Exemplo:
+// console.assert(1 === 2, "Teste 1 === 2");
+// console.assert(1 + 1 === 3, "Teste 1 + 1 = 2");
+
---
+++ m03/01-hello-redux/notes.md
@@ -0,0 +1,7 @@
+## Servidor estático com Python
+
+Exemplo executando na porta 9001
+
+```
+$ python -m SimpleHTTPServer 9001
+```
