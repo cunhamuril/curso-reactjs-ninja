6206ff0932572f6963960da106bf356690f4c630
Murilo <murilo.sant@hotmail.com>
2020-04-15 23:08:25 -0300

    feat(todoslist): mostrar todos filtrados

--- m03/03-todo-list/src/components/TodosList/index.jsx
+++ m03/03-todo-list/src/components/TodosList/index.jsx
@@ -4,10 +4,15 @@ import { connect } from "react-redux";
 import { Container } from "./styles";
 
 import { toggleTodo } from "../../reduxFlow/reducers/todos/actionCreators";
+import {
+  SHOW_ALL,
+  SHOW_COMPLETED,
+  SHOW_ACTIVE,
+} from "../../reduxFlow/reducers/visibilityFilter/actions";
 
-const TodosList = ({ todos, handleToggleTodo }) => (
+const TodosList = ({ todos, activeFilter, handleToggleTodo }) => (
   <Container>
-    {todos.map((todo) => (
+    {getVisibleTodos(todos, activeFilter).map((todo) => (
       <li
         key={todo.id}
         className={todo.completed ? "completed" : ""}
@@ -19,8 +24,35 @@ const TodosList = ({ todos, handleToggleTodo }) => (
   </Container>
 );
 
+const getVisibleTodos = (todos, activeFilter) => {
+  // Feature do ES6 -> estrutura condicional feito com objeto
+  const filterItems = {
+    [SHOW_ALL]: todos,
+    [SHOW_COMPLETED]: todos.filter((todo) => todo.completed),
+    [SHOW_ACTIVE]: todos.filter((todo) => !todo.completed),
+  };
+
+  return filterItems[activeFilter];
+
+  /**
+   * Essa estrutura acima, é como se fosse este switch abaixo
+   */
+  // switch (activeFilter) {
+  //   case SHOW_ALL:
+  //     return todos;
+  //   case SHOW_COMPLETED:
+  //     return todos.filter((todo) => todo.completed);
+  //   case SHOW_ACTIVE:
+  //     return todos.filter((todo) => !todo.completed);
+  //   default:
+  //     return todos;
+  // }
+};
+
+// Redux
 const mapStateToProps = (state) => ({
   todos: state.todos,
+  activeFilter: state.visibilityFilter,
 });
 
 const mapDispatchToProps = (dispatch) => ({
