e3c07d83f620017571dfd2bf042702eb2aad0784
Murilo <murilo.sant@hotmail.com>
2020-04-15 21:14:24 -0300

    refactor(actions): renomeação das actions

--- m03/03-todo-list/src/reduxFlow/reducers/index.js
+++ m03/03-todo-list/src/reduxFlow/reducers/index.js
@@ -3,6 +3,10 @@
 import todos from "./todos";
 import visibilityFilter from "./visibilityFilter";
 
+/**
+ * Função que mostra como funciona o combineReducers por baixo dos panos
+ * @param {Object} reducers reducers que irão ser combinados
+ */
 const combineReducers = (reducers) => (state = {}, action) => {
   return Object.keys(reducers).reduce((nextState, key) => {
     nextState[key] = reducers[key](state[key], action);
