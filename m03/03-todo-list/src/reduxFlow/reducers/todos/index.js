f7cbc75b7b40e1bf5b384c7461841bcfd97f5593
Murilo Cunha <murilo.sant@hotmail.com>
2020-04-19 03:47:35 -0300

    refactor(create-reducer): add default function createReducer

--- m03/03-todo-list/src/reduxFlow/reducers/todos/index.js
+++ m03/03-todo-list/src/reduxFlow/reducers/todos/index.js
@@ -1,29 +1,53 @@
 import { ADD_TODO, TOGGLE_TODO } from "./actions";
+import createReducer from "../createReducer";
 
 export const initialState = [];
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return state.concat({
-        id: action.payload.id,
-        text: action.payload.text,
-        completed: false,
-      });
+const todos = createReducer(initialState, {
+  [ADD_TODO]: (state, action) =>
+    state.concat({
+      id: action.payload.id,
+      text: action.payload.text,
+      completed: false,
+    }),
 
-    case TOGGLE_TODO:
-      return state.map((todo) => {
-        if (todo.id !== action.payload.id) {
-          return todo;
-        }
+  [TOGGLE_TODO]: (state, action) =>
+    state.map((todo) => {
+      if (todo.id !== action.payload.id) {
+        return todo;
+      }
 
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      });
+      return {
+        ...todo,
+        completed: !todo.completed,
+      };
+    }),
+});
 
-    default:
-      return state;
-  }
-};
+// const todos = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return state.concat({
+//         id: action.payload.id,
+//         text: action.payload.text,
+//         completed: false,
+//       });
+
+//     case TOGGLE_TODO:
+//       return state.map((todo) => {
+//         if (todo.id !== action.payload.id) {
+//           return todo;
+//         }
+
+//         return {
+//           ...todo,
+//           completed: !todo.completed,
+//         };
+//       });
+
+//     default:
+//       return state;
+//   }
+// };
+
+export default todos;
