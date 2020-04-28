0f3521e747f27d29bc53ed1a1da5c3c4472d5874
Murilo Cunha <murilo.sant@hotmail.com>
2020-04-28 19:52:36 -0300

    feat(store): app initialState

--- m03/04-todo-list-get-cep/src/index.js
+++ m03/04-todo-list-get-cep/src/index.js
@@ -7,7 +7,26 @@ import App from "./App";
 
 import reducer from "./reduxFlow/reducers";
 
-const store = createStore(reducer);
+const initialState = {
+  todos: [
+    {
+      text: "auto",
+      id: "123",
+      completed: false,
+    },
+  ],
+
+  address: {
+    address: "Rua Tal",
+    city: "cidade",
+    state: "PR",
+    code: "10101-123",
+    district: "Bairro",
+    ok: true,
+  },
+};
+
+const store = createStore(reducer, initialState);
 
 store.subscribe(() => {
   console.log(store.getState());
