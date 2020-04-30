43c1bc721a723fee50cb4454b8534ce849a40f55
Murilo Cunha <murilo.sant@hotmail.com>
2020-04-29 22:56:22 -0300

    feat(index): redux with middlewares

--- m03/04-todo-list-get-cep/src/index.js
+++ m03/04-todo-list-get-cep/src/index.js
@@ -1,36 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./App";
 
 import reducer from "./reduxFlow/reducers";
 
-const initialState = {
-  todos: [
-    {
-      text: "auto",
-      id: "123",
-      completed: false,
-    },
-  ],
-
-  address: {
-    address: "Rua Tal",
-    city: "cidade",
-    state: "PR",
-    code: "10101-123",
-    district: "Bairro",
-    ok: true,
-  },
+// Estrutura de um middleware do Redux
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  console.log("LOGGER::will dispatch:", action);
+  const nextAction = next(action);
+  console.log("LOGGER::next action:", nextAction);
+  return nextAction;
 };
 
-const store = createStore(reducer, initialState);
+const thunk = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action === "function") {
+    return action(dispatch, getState);
+  }
+  return next(action);
+};
 
-store.subscribe(() => {
-  console.log(store.getState());
-});
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+
+// -------------------
+store.dispatch(lazyAction());
+function lazyAction() {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      dispatch({
+        type: "todos:ADD_TODO",
+        payload: {
+          text: "Lazy Action",
+          id: "123",
+        },
+      });
+    }, 2000);
+  };
+}
+// -------------------
+
+const renderState = () => {
+  console.log("state:", store.getState());
+};
+
+store.subscribe(renderState);
+renderState();
 
 ReactDOM.render(
   <React.StrictMode>
