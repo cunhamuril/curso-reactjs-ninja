c0a74011e7b1b9db4d553f223c4b097d810323d3
Murilo Cunha <murilo.sant@hotmail.com>
2020-05-03 16:16:06 -0300

    feat(configurestore): store enhancer and redux devtools

--- m03/04-todo-list-get-cep/src/reduxFlow/conrigureStore/index.js
+++ m03/04-todo-list-get-cep/src/reduxFlow/conrigureStore/index.js
@@ -1,30 +1,19 @@
 import "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers";
 
 export default ({ initialState } = {}) => {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(logger, thunk)
-  );
+  const enhancer = compose(applyMiddleware(thunk), logger());
+  const store = createStore(rootReducer, initialState, enhancer);
 
   return store;
 };
 
 // Middlewares
-const logger = ({ dispatch, getState }) => (next) => (action) => {
-  console.group(`LOGGER->${action.type}`); // feature para agrupar logs
-  console.log("will dispatch:", action);
-  console.log("state", getState());
-
-  const nextAction = next(action);
-
-  console.log("next state", getState());
-  console.groupEnd(`LOGGER->${action.type}`);
-
-  return nextAction;
-};
+const logger = () =>
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (x) => x;
 
 const thunk = ({ dispatch, getState }) => (next) => (action) => {
   if (typeof action === "function") {
