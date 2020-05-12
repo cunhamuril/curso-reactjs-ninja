6fe73d0c84a69dfa5d6c8979a9da23666d567601
Murilo Cunha <murilo.sant@hotmail.com>
2020-05-11 21:47:15 -0300

    feat(firebase): added firebase, configurate and read data from database

--- m03/05-reactflix/src/index.js
+++ m03/05-reactflix/src/index.js
@@ -4,11 +4,37 @@ import { Provider } from "react-redux";
 
 import App from "./App";
 
+import firebase from "./services/firebase";
+
 import configureStore from "./reduxFlow/configureStore";
 
 import GlobalStyles from "./theme/global";
 
 const store = configureStore();
+
+const db = firebase.database();
+const videos = db.ref("videos");
+
+/**
+ * Faz leitura toda vez que os dados forem modificados
+ */
+// videos.on(
+//   "value",
+//   (snapshot) => {
+//     console.log("snapshot:", snapshot.val());
+//   },
+//   (error) => {
+//     console.error("error:", error);
+//   }
+// );
+
+/**
+ * Faz leitura apenas uma vez
+ */
+videos
+  .once("value")
+  .then((snapshot) => console.log("snapshot:", snapshot.val()))
+  .catch((err) => console.error(err));
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ m03/05-reactflix/src/services/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase";
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyDpXDKCNin7nklN_FNIrNvd4i-REEfvmXw",
+  authDomain: "reactflix-93036.firebaseapp.com",
+  databaseURL: "https://reactflix-93036.firebaseio.com",
+  projectId: "reactflix-93036",
+  storageBucket: "reactflix-93036.appspot.com",
+  messagingSenderId: "265405971096",
+  appId: "1:265405971096:web:fd76d2965678d49b8f2eed",
+});
