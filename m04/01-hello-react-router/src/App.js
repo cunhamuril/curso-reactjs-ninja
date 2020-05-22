d170c9334ef3dd972ef942e99db35099921ff4d5
Murilo Cunha <murilo.sant@hotmail.com>
2020-05-21 22:28:56 -0300

    feat: created more routes

--- m04/01-hello-react-router/src/App.js
+++ m04/01-hello-react-router/src/App.js
@@ -4,13 +4,27 @@ import { Link, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Link to="/sobre">Sobre</Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sobre">Sobre</Link>
+        </li>
+        <li>
+          <Link to="/contato">Contato</Link>
+        </li>
+      </ul>
 
+      <Route path="/" exact component={Home} />
       <Route path="/sobre" component={Sobre} />
+      <Route path="/contato" component={Contato} />
     </div>
   );
 }
 
+const Home = () => <h1>Home</h1>;
 const Sobre = () => <h1>Sobre</h1>;
+const Contato = () => <h1>Contato</h1>;
 
 export default App;
