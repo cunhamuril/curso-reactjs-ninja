31eff449f9f8426d0101e996f34706264a68004f
Murilo Cunha <murilo.sant@hotmail.com>
2020-07-06 23:19:28 -0300

    feat(react-zzaria): basic standard config

--- m04/02-reactzzaria/sistema-pedidos/src/ErrorBoundary.jsx
+++ m04/02-reactzzaria/sistema-pedidos/src/ErrorBoundary.jsx
@@ -1,24 +1,24 @@
-import { PureComponent } from "react";
+import { PureComponent } from 'react'
 
 /**
  * Este componente basicamente funciona como se fosse um Try Catch de erros em componentes
  */
 
 class ErrorBoundary extends PureComponent {
-  state = { hasError: false };
+  state = { hasError: false }
 
   static getDerivedStateFromError() {
-    return { hasError: true };
+    return { hasError: true }
   }
 
   componentDidCatch(error, info) {
-    console.log("error: ", error);
-    console.log("info: ", info.componentStack);
+    console.log('error: ', error)
+    console.log('info: ', info.componentStack)
   }
 
   render() {
-    return this.props.children(this.state.hasError);
+    return this.props.children(this.state.hasError)
   }
 }
 
-export default ErrorBoundary;
+export default ErrorBoundary
