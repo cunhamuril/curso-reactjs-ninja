c288e00cfdb551235ebc161df26cd601361b1f8c
Murilo Cunha <murilo.sant@hotmail.com>
2020-07-06 22:10:11 -0300

    feat(error-boundary): logging errors messages

--- m04/02-reactzzaria/sistema-pedidos/src/components/ErrorBoundary.jsx
+++ m04/02-reactzzaria/sistema-pedidos/src/components/ErrorBoundary.jsx
@@ -1,10 +1,19 @@
 import { PureComponent } from "react";
 
+/**
+ * Este componente basicamente funciona como se fosse um Try Catch de erros em componentes
+ */
+
 class ErrorBoundary extends PureComponent {
   state = { hasError: false };
 
   static getDerivedStateFromError() {
     return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.log("error: ", error);
+    console.log("info: ", info.componentStack);
   }
 
   // render() {
