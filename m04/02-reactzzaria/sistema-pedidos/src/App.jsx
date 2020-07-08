0a2e81fac16869882414092cf1b9bb4d632e130e
Murilo Cunha <murilo.sant@hotmail.com>
2020-07-07 22:17:51 -0300

    feat(react-zzaria): basic ui config

--- m04/02-reactzzaria/sistema-pedidos/src/App.jsx
+++ m04/02-reactzzaria/sistema-pedidos/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 import Routes from './routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
   );
 }
 
