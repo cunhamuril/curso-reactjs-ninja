e364fe0340ef90bbbe728fc56ddb61c90ad35782
Murilo Cunha <murilo.sant@hotmail.com>
2020-07-09 22:36:29 -0300

    feat(hooks): useState

---
+++ m04/03-hooks/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+const App = () => (
+  <>
+    <CounterClass />
+    <CounterFunction />
+  </>
+);
+
+// Componente de classe
+class CounterClass extends React.PureComponent {
+  state = {
+    counter: 0,
+  };
+
+  render() {
+    return (
+      <Counter
+        title="Class"
+        counter={this.state.counter}
+        increment={() => {
+          this.setState({
+            counter: this.state.counter + 1,
+          });
+        }}
+        decrement={() => {
+          this.setState({
+            counter: this.state.counter - 1,
+          });
+        }}
+      />
+    );
+  }
+}
+
+// Componente de função
+function CounterFunction() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <Counter
+      title="Function"
+      counter={counter}
+      increment={() => setCounter(counter + 1)}
+      decrement={() => setCounter(counter - 1)}
+    />
+  );
+}
+
+// Componente principal
+const Counter = ({ title, counter, increment, decrement }) => (
+  <div>
+    <h1 style={styles.title}>{title}</h1>
+    <h1>{counter}</h1>
+    <button onClick={decrement}>-</button>
+    <button onClick={increment}>+</button>
+  </div>
+);
+
+const styles = {
+  title: { backgroundColor: "#AAA", padding: 4, marginBottom: 4 },
+};
+
+export default App;
