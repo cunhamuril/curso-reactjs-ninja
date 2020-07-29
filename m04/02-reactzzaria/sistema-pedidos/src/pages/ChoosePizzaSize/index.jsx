540c0a5bdfa20140aad5c611623ca7172064cd73
Murilo Cunha <murilo.sant@hotmail.com>
2020-07-28 22:00:56 -0300

    feat(fake data): organization

--- m04/02-reactzzaria/sistema-pedidos/src/pages/ChoosePizzaSize/index.jsx
+++ m04/02-reactzzaria/sistema-pedidos/src/pages/ChoosePizzaSize/index.jsx
@@ -3,6 +3,8 @@ import { Typography, Grid } from '@material-ui/core';
 
 import { AuthContext } from '../../contexts/Auth';
 
+import pizzaSizes from '../../mock/pizzaSizes';
+
 import {
   Divider,
   PaperPizza,
@@ -15,30 +17,6 @@ import {
 function singularOrPlural(amount, singular, plural) {
   return amount === 1 ? singular : plural;
 }
-
-const pizzaSizes = [
-  {
-    id: 0,
-    name: 'Pequena',
-    size: 28,
-    slices: 2,
-    flavours: 1,
-  },
-  {
-    id: 1,
-    name: 'Média',
-    size: 30,
-    slices: 6,
-    flavours: 2,
-  },
-  {
-    id: 2,
-    name: 'Grande',
-    size: 32,
-    slices: 8,
-    flavours: 3,
-  },
-];
 
 function ChoosePizzaSize() {
   const { user } = useContext(AuthContext);
